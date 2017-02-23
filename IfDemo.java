af8a0973465032cdf2ba66a65e1bd65b43999a34
Nagenderbabu <nagenderbabu34@gmail.com>
2017-02-23 20:39:20 +0530

    Add files via upload

---
+++ IfDemo.java
@@ -0,0 +1,23 @@
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Declaring variable age and initializing it to value 5
+		int age=5;
+		// checking if age is greater or equal to 18 
+		if(age>=18)
+		{
+			// this block executes if age is greater or equal to 18
+			System.out.println(" eligible to vote");
+		}
+		else{
+			//this block executes if age is less than 18
+			System.out.println("Not eligible to vote");
+		}	
+		// if block ends 
+		System.out.println("Executed Successfully");
+	}
+
+}
